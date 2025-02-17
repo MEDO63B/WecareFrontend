b14cb5292df400b3b85255a8f7399773da444a7d
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2025-02-18 01:58:32 +0200

    Setup: Testing Enviroment

--- wecareapp/src/app/services/user/user.service.ts
+++ wecareapp/src/app/services/user/user.service.ts
@@ -8,8 +8,8 @@ export class UserService {
 
     isLogin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
     isAdmin:  BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-    isDoctor:  BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    isUser:  BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    isDoctor:  BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+    isUser:  BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
 constructor() {
 
